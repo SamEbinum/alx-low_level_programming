a52892914fe043d1a5c53b068d401fdefbca69fa
SamEbinum <samuelebinum39@gmail.com>
2023-03-17 07:51:08 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,12 @@
+{
+	char low;
+
+	for(low = 'a'; low <= 'z'; low++)
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+
+	for(low = 'A'; low <= 'Z'; low++)
+	for (low = 'A'; low <= 'Z'; low++)
+		putchar(low);
+
+	putchar('\n');
