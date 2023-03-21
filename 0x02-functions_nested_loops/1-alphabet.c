d9ac951e6a334e14deb607a081c4e66450845099
SamEbinum <samuelebinum39@gmail.com>
2023-03-21 01:42:12 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+
+ */
+int main(void)
+{
+		char low;
+	char low;
+
+		for (low = 'a'; low <= 'z'; low++)
+			putchar(low);
+		return (1);
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+
+	putchar('\n');
+
+	return (0);
+}
