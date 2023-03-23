9266c46e828016703cc76ab7a17868652da11cb0
SamEbinum <samuelebinum39@gmail.com>
2023-03-23 05:30:53 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * mul - Multiples two integers
  * @a: The first integer to be multiplied
@@ -13,4 +12,5 @@ int mul(int a, int b)
 	printf("%d\n", mul(98, 1024));
 	printf("%d\n", mul(-402, 4096));
 	return (0);
-}
+	return (a * b);
+}}
